8938afe528dbcdb787426f9636b11b92cba6f66f
Kohki SHIKATA <kohki.shikata@gmail.com>
2017-01-08 21:06:29 +0900

    implement name input

--- dev/js/app.js
+++ dev/js/app.js
@@ -6,3 +6,8 @@ var page = $('#preview > .page');
 var pagew = page.innerWidth();
 
 $(page).css("height",pagew * 1.41428);
+//名前のリアルタイム入力
+$('#name').on("keyup",function(){
+  var name = $(this).val();
+  $('#preview .name-person').text(name);
+});
